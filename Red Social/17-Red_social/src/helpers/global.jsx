3ea4fc752b78df0f876d86211ff47093c1f75d9d
joel <100796026+Th3Ninj4@users.noreply.github.com>
2023-09-11 17:46:41 -0600

    delete

--- Red Social/17-Red_social/src/helpers/global.jsx
+++
@@ -1,3 +0,0 @@
-export const global = {
-	url: "http://localhost:8080/api/"
-};
--- Red Social/api-red-social/arrancar-proyecto.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /k "cd C:\Juegos\MongoDB\Server\4.2\bin && mongod.exe --dbpath C:\Users\Joel\Pictures\datamdb\db"
-start cmd.exe /k "cd C:\Users\Joel\Desktop\Red Social\api-red-social && npm start"
